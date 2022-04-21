9577a185486b7b3d6928edfb8fe4e5354bf15a1a
SakshamJain418 <97030409+SakshamJain418@users.noreply.github.com>
2022-04-21 10:28:08 +0530

    Create help.js

---
+++ help.js
@@ -0,0 +1,11 @@
+function helpFn() {
+    console.log(`List of all the commands are :- 
+                    1. saksham tree
+                    2. saksham organize
+                    3. saksham help
+    `)
+}
+
+module.exports = {
+    helpKey : helpFn
+}
